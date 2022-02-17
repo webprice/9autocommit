482626c076576f2f32112e9bb8fcd6ef5b3a55fe
root <out455@ya.ru>
2022-02-17 14:45:59 +0000

    upload to github

--- rand_gen.py
+++ rand_gen.py
@@ -2,16 +2,23 @@ import time
 import random
 import string
 
-
+'''generate random number, that will declare how many letters 
+will be added to the text.txt file. Each time we change the text.txt file
+we can made a commit and push it to the reote repo
+which will be considered as a github contribution'''
 x = random.randrange(10)
 random_letter = random.choices(string.ascii_letters,k=x)
 
+
+'''function which actually adding those letters t the text.txt file'''
 def random_letter_write(letters):
     try:
         streng = ""
         for l in letters:
             streng += l
         time.sleep(1)
+'''opening the text.txt file, but if it doesn't exist, it will be created,
+this is why were using a+ attribute'''
         with  open("text.txt",'a+') as f:
             f.write(streng)
             time.sleep(1)
